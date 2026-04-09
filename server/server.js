d0c34d06d567c9e2177ec66c101a59a9c7b3419b
Aarsh Singh <starfire9599@gmail.com>
2026-04-09 20:57:00 +0530

    feat: user model, apis for register, login and get user data, auth middleware

--- server/server.js
+++ server/server.js
@@ -1,21 +1,23 @@
-import express from "express"
-import 'dotenv/config'
-import cors from "cors"
-import connectDB from "./configs/db.js"
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
 
-const app = express()
+const app = express();
 
-await connectDB()
+await connectDB();
 
 // Middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => res.send("Server is Live!"))
+app.get("/", (req, res) => res.send("Server is Live!"));
+app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
